b3a889971ebd34f9b185dec3f79aac8b309ac6a4
BHARATAPU KARTHIK <69613029+danakarthik@users.noreply.github.com>
2022-08-19 23:40:34 +0530

    Update bfs.java

--- bfs.java
+++ bfs.java
@@ -1,64 +1,67 @@
 import java.util.*;
-public class Solution {
-    public static ArrayList < Integer > bfsOfGraph(int V, ArrayList < ArrayList < Integer >> adj) {
-
-        ArrayList < Integer > bfs = new ArrayList < > ();
-        boolean vis[] = new boolean[V+1];
-        Queue < Integer > q = new LinkedList < > ();
-        for(int i=0;i<5;i++)
-        {
-            if(vis[i]==false)
-            {
-                  q.add(i);
-                     vis[i] = true;
-            }
-        while (!q.isEmpty()) {
-            Integer node = q.poll();
-            bfs.add(node);
-
-            // Get all adjacent vertices of the dequeued vertex s
-            // If a adjacent has not been visited, then mark it
-            // visited and enqueue it
-            for (Integer it: adj.get(node)) {
-                if (vis[it] == false) {
-                    vis[it] = true;
-                    q.add(it);
-                }
-            }
-        }
-        }
-        return bfs;
-
-       
-    }
-
-    static void printAns(ArrayList < Integer > ans) {
-        for (int i = 0; i < ans.size(); i++) {
-            System.out.print(ans.get(i) + " ");
-        }
-    }
+public class MyClass {
     public static void main(String args[]) {
         Scanner in=new Scanner(System.in);
-        int u,v;
-
-        ArrayList < ArrayList < Integer >> adj = new ArrayList < > ();
-        for (int i = 0; i < 5; i++) {
-            adj.add(new ArrayList < > ());
-        }
-     
-   for(int i=0;i<5;i++)
+        int n=in.nextInt();
+        int v,u;
+       
+     ArrayList<ArrayList<Integer>>adj=new ArrayList<ArrayList<Integer>>();
+    
+     for(int i=0;i<5;i++)
+     {
+         adj.add(new ArrayList<Integer>());
+     }
+     for(int i=0;i<n;i++)
      {
          u=in.nextInt();
          v=in.nextInt();
      adj.get(u).add(v);
      adj.get(v).add(u);
-       }
+     }
     
-
-        ArrayList < Integer > ans = bfsOfGraph(5, adj);
-        printAns(ans);
-
-
-
+    /* System.out.println(adj);
+      for(int i=0;i<adj.size();i++)
+      {
+          System.out.print(i+" edges->  ");
+          for(int j=0;j<adj.get(i).size();j++)
+          {
+              
+              System.out.print(adj.get(i).get(j)+" ");
+          }
+          System.out.println();
+      }*/
+//--------------------------------------------------------bfs traversal--------------//-------------------------------------------------------------------------------------------------------------------------
+          ArrayList<Integer>bfs=new ArrayList<>();
+          boolean[] vis=new boolean[n+1];
+          Queue<Integer>que=new LinkedList<>();
+          for(int i=1;i<n;i++)
+          {
+           
+              if(vis[i]==false)
+              {
+                 
+                  que.add(i);
+                   vis[i]=true;
+              while(!que.isEmpty())
+              {
+                  Integer node=que.poll();
+                  bfs.add(node);
+                  for(Integer it:adj.get(node))
+                  {
+                      if(vis[it]==false)
+                      {
+                          vis[it]=true;
+                        que.add(it);
+                      }
+                 
+                      
+                  }
+              }
+              
+          }
+          }
+          System.out.println(bfs);
+//==================================================================================bfs=======           
+      
     }
 }
